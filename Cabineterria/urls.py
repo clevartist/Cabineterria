adc0e0b704643a4dcb439b5950fd19c4c0d49e7b
clevartist <isomdez@gmail.com>
2025-03-08 17:08:35 +0200

    o

--- Cabineterria/urls.py
+++ Cabineterria/urls.py
@@ -5,8 +5,9 @@ from .views import Home, CabinetView, BuildCabinet, Login, Signup, Logout
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home.as_view(), name='home'),
-    re_path(r'^cabinet/(?P<cabinet_path>.+)/$', CabinetView.as_view(), name='cabinet'), #although i noticed i can remove "r", "^" and "/$" and it still will work
-    path('build/', BuildCabinet.as_view(), name='build_cabinet'),
+    re_path(r'^cabinet/(?P<cabinet_path>.+)/build/$', BuildCabinet.as_view(), name='build_subcabinet'),
+    re_path(r'^cabinet/(?P<cabinet_path>.+)/$', CabinetView.as_view(), name='cabinet'),
+    path('build/', BuildCabinet.as_view(), name="build_cabinet"),
     path('login/', Login.as_view(), name='login'),
     path('signup/', Signup.as_view(), name='signup'),
     path('logout/', Logout.as_view(), name='logout'),
