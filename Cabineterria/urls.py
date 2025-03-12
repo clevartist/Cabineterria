e3cc11695ff6b84223c55facd95f638d014f638c
Clev <105460616+clevartist@users.noreply.github.com>
2025-03-12 13:40:06 +0200

    Update urls.py

--- Cabineterria/urls.py
+++ Cabineterria/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     re_path(r'^cabinet/(?P<cabinet_path>.+)/build/$', BuildCabinet.as_view(), name='build_subcabinet'),
     re_path(r'^cabinet/(?P<cabinet_path>.+)/$', CabinetView.as_view(), name='cabinet'),
+    path('cabinet/<int:cabinet_id>/', Answer.as_view(), name='answer_questions'),
     path('build/', BuildCabinet.as_view(), name="build_cabinet"),
     path('login/', Login.as_view(), name='login'),
     path('signup/', Signup.as_view(), name='signup'),
     path('logout/', Logout.as_view(), name='logout'),
-    path('profile/<str:username>/', Profile.as_view(), name='profile')
+    path('profile/<str:username>/', Profile.as_view(), name='profile'),
 ]
