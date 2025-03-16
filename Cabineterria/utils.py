2eee40b5b3a1aee02a2a46985fb9d77fbdd7176f
clevartist <isomdez@gmail.com>
2025-03-16 13:06:21 +0200

    redesign

---
+++ Cabineterria/utils.py
@@ -0,0 +1,33 @@
+from django.shortcuts import get_object_or_404
+from .models import CabinetModel, UserCabinetStatus
+
+def get_cabinet_from_path(path):
+    """Resolve cabinet hierarchy from URL path"""
+    if not path:
+        return None
+    
+    current = None
+    for name in path.split('/'):
+        try:
+            current = CabinetModel.objects.get(
+                name=name, 
+                parent=current if current else None
+            )
+        except CabinetModel.DoesNotExist:
+            return None
+    return current
+
+def validate_cabinet_access(user, cabinet):
+    """Check if user can access cabinet"""
+    if not cabinet.requires_questions:
+        return True
+        
+    if not user.is_authenticated:
+        return False
+        
+    status = UserCabinetStatus.objects.filter(
+        user=user, 
+        cabinet=cabinet
+    ).first()
+    
+    return not (status and status.locked)
